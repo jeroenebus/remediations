c5a8982744c7a2ba9651dc839800e149684109bb
Jeroen Ebus <jeroen.ebus@peoplerock.nl>
2023-05-25 21:34:29 +0200

    First version of the local administrator creation remediation script for LAPS.

---
+++ Create_Local_Admin/Detect_Local_Admin.ps1
@@ -0,0 +1,27 @@
+<#
+.Synopsis
+    Detection script to detect if the desired local admin is on the device
+.DESCRIPTION
+    This detection script is part of a remediation for creating a local administrator
+.EXAMPLE
+    Creates a local administrator with a specific name and random password, this can be used with LAPS where the password will be set through a policy
+.NOTES
+    Filename: Check-Device.ps1
+    Author: Jeroen Ebus (https://manage-the.cloud) 
+    Modified date: 2023-05-25
+    Version 1.0 - Release notes/details
+#>
+
+# Dection script! Based on https://cloudinfra.net/create-a-local-admin-using-intune-and-powershell/
+
+$userName = "cloud-local-admin"
+$Userexist = (Get-LocalUser).Name -Contains $userName
+
+if ($userexist) { 
+    Write-Host "$userName exist" 
+    Exit 0
+} 
+Else {
+    Write-Host "$userName does not Exists"
+    Exit 1
+}
---
+++ Create_Local_Admin/Remediate_Local_Admin.ps1
@@ -0,0 +1,55 @@
+<#
+.Synopsis
+    Remediation script to detect if the desired local admin is on the device
+.DESCRIPTION
+    This remediation script for creating a local administrator
+.EXAMPLE
+    Creates a local administrator with a specific name and random password, this can be used with LAPS where the password will be set through a policy
+.NOTES
+    Filename: Check-Device.ps1
+    Author: Jeroen Ebus (https://manage-the.cloud) 
+    Modified date: 2023-05-25
+    Version 1.0 - Release notes/details
+#>
+
+# Function created with the help of https://www.sharepointdiary.com/2020/04/powershell-generate-random-password.html
+
+Function Get-RandomPassword {
+    #define parameters
+    param([int]$PasswordLength = 10)
+ 
+    #ASCII Character set for Password
+    $CharacterSet = @{
+        Uppercase   = (97..122) | Get-Random -Count 10 | % { [char]$_ }
+        Lowercase   = (65..90)  | Get-Random -Count 10 | % { [char]$_ }
+        Numeric     = (48..57)  | Get-Random -Count 10 | % { [char]$_ }
+        SpecialChar = (33..47) + (58..64) + (91..96) + (123..126) | Get-Random -Count 10 | % { [char]$_ }
+    }
+ 
+    #Frame Random Password from given character set
+    $StringSet = $CharacterSet.Uppercase + $CharacterSet.Lowercase + $CharacterSet.Numeric + $CharacterSet.SpecialChar
+ 
+    -join (Get-Random -Count $PasswordLength -InputObject $StringSet)
+}
+ 
+#Call the function to generate random password of 8 characters
+Get-RandomPassword -PasswordLength 8
+ 
+#Sample Output: glx`FC>Y
+
+# Actual remediation! Based on https://cloudinfra.net/create-a-local-admin-using-intune-and-powershell/
+
+$userName = "cloud-local-admin"
+$userexist = (Get-LocalUser).Name -Contains $userName
+$password = Get-RandomPassword
+
+if ($userexist -eq $false) {
+    try { 
+        New-LocalUser -Name $username -Description "Local adminsitrator account managed through LAPS" -Password $password
+        Exit 0
+    }   
+    Catch {
+        Write-error $_
+        Exit 1
+    }
+} 
