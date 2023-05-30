9985313d3d7bf5eb69fb44d10fa360737d590165
Jeroen Ebus <jeroen.ebus@peoplerock.nl>
2023-05-30 21:50:19 +0200

    Replaced % for ForEach-Object

--- Create_Local_Admin/Remediate_Local_Admin.ps1
+++ Create_Local_Admin/Remediate_Local_Admin.ps1
@@ -20,10 +20,10 @@ Function Get-RandomPassword {
  
     #ASCII Character set for Password
     $CharacterSet = @{
-        Uppercase   = (97..122) | Get-Random -Count 10 | % { [char]$_ }
-        Lowercase   = (65..90)  | Get-Random -Count 10 | % { [char]$_ }
-        Numeric     = (48..57)  | Get-Random -Count 10 | % { [char]$_ }
-        SpecialChar = (33..47) + (58..64) + (91..96) + (123..126) | Get-Random -Count 10 | % { [char]$_ }
+        Uppercase   = (97..122) | Get-Random -Count 10 | ForEach-Object { [char]$_ }
+        Lowercase   = (65..90)  | Get-Random -Count 10 | ForEach-Object { [char]$_ }
+        Numeric     = (48..57)  | Get-Random -Count 10 | ForEach-Object { [char]$_ }
+        SpecialChar = (33..47) + (58..64) + (91..96) + (123..126) | Get-Random -Count 10 | ForEach-Object { [char]$_ }
     }
  
     #Frame Random Password from given character set
